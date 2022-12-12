de08648074acc80c586aa60b166458ca79f014e3
mongpt <mongphantan@gmail.com>
2022-12-12 21:04:26 +0200

    ok

---
+++ 01.js
@@ -0,0 +1,18 @@
+// step 1
+document.addEventListener('submit', async function(evt) {
+    evt.preventDefault();
+    const inputValue = document.querySelector('#query').value
+    try {
+        const proxy = 'https://api.allorigins.win/get?url=';
+        const search = 'https://api.tvmaze.com/search/shows?q='+ inputValue;
+        const url = proxy + encodeURIComponent(search);
+
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Invalid input!');
+        const result = await response.json();
+        const dataFromAPI = JSON.parse(result.contents);
+        console.log(dataFromAPI)
+    } catch (e) {
+        console.log('error', e);
+    }
+});
---
+++ 04.js
@@ -0,0 +1,53 @@
+// step 4
+document.addEventListener('submit', async function(evt) {
+    evt.preventDefault();
+    const inputValue = document.querySelector('#query').value;
+    try {
+        const proxy = 'https://api.allorigins.win/get?url=';
+        const search = 'https://api.tvmaze.com/search/shows?q='+ inputValue;
+        const url = proxy + encodeURIComponent(search);
+
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Invalid input!');
+        const result = await response.json();
+        const dataFromAPI = JSON.parse(result.contents);
+
+        const results = document.createElement('h1');
+        results.innerHTML = 'Results'
+        document.body.appendChild(results)
+
+        for (let item of dataFromAPI) {
+            const container = document.createElement('div');
+            const h2 = document.createElement('h2');
+            h2.innerHTML = item['show']['name'];
+            container.appendChild(h2);
+
+            const image = document.createElement('img');
+            image.src = item['show']['image'] ?
+                item['show']['image']['medium'] :
+                'https://via.placeholder.com/200x300?text=default image'
+            image.alt = item['show']['name'];
+            container.appendChild(image);
+
+            const genres = document.createElement('h3');
+            genres.innerHTML = item['show']['genres'].join(' | ');
+            container.appendChild(genres);
+
+            const description = document.createElement('p');
+            description.innerHTML = item['show']['summary'] ?
+                `${item['show']['summary'].slice(0, 800)}...` :
+                item['show']['summary']
+            container.appendChild(description);
+
+            const link = document.createElement('a');
+            link.href = item['show']['url'];
+            link.target = "_blank";
+            link.innerHTML = 'Learn more';
+            container.appendChild(link);
+            document.body.appendChild(container)
+        }
+
+    } catch (error) {
+        console.log('error', error);
+    }
+});
